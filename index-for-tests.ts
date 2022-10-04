8d870471c868569355ce860c8e4c1685116307b7
diogoneves07 <ndiogo778@gmail.com>
2022-10-04 09:36:48 -0300

    Bentive mascot logo moved to assets folder

--- index-for-tests.ts
+++ index-for-tests.ts
@@ -1,15 +1,17 @@
 import { render, _ } from "./src/main";
 
-_("Counter", ({ click$, i }) => {
-  let count = 0;
+_("Counter", ({ click$, p }) => {
+  let count = p.start || 0;
+  console.log({ ...p });
 
   click$(() => count++);
 
   return () => `button[Cliked: ${count}]`;
 });
 
-_("App", () => {
-  return () => `Counter example: Counter[]`;
+_("App", ({ defineProps }) => {
+  defineProps("a", { start: 90 });
+  return () => `Counter example: Counter_a[]`;
 });
 
 // <div>Hello world!</div>
