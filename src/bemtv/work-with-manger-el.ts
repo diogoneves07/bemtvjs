3a651a3040309212c4a0a9af8eaef3993060bba1
diogoneves07 <ndiogo778@gmail.com>
2022-10-20 08:47:35 -0300

    fix: should apply the style after add the class to the element

--- src/bemtv/work-with-manger-el.ts
+++ src/bemtv/work-with-manger-el.ts
@@ -8,9 +8,10 @@ export function getManagerElData(m: ManagerEl) {
 }
 
 export function applyElementCSS(el: Element, args: CSSInJSParameters) {
-  BEMTEVI_CSS_IN_JS.applyLastCSSCreated();
   const c = BEMTEVI_CSS_IN_JS.gooberCSS(...args);
   el.classList.add(c);
+
+  BEMTEVI_CSS_IN_JS.applyLastCSSCreated();
 
   return c;
 }
