57519ff4cab55f4a317d882901f43f3cf9196fdb
diogoneves07 <ndiogo778@gmail.com>
2022-10-21 17:37:09 -0300

    test: replacement of elements and reapplication of attributes

---
+++ __tests__/bemtv/it-property.test.ts
@@ -0,0 +1,99 @@
+import { Component } from "../../src/main";
+
+describe("ManagerEl.it property", () => {
+  it("Should use #app element in ManagerEl", (done) => {
+    const div = document.createElement("div");
+    div.id = "app";
+    document.body.appendChild(div);
+
+    Component("App1", ({ el }) => {
+      const appEl = el("#app");
+
+      setTimeout(() => {
+        expect(appEl.it).toBe(div);
+        done();
+      }, 200);
+
+      return () => ``;
+    }).render();
+  });
+
+  it("Should use div element in ManagerEl", (done) => {
+    const div = document.createElement("div");
+
+    document.body.appendChild(div);
+
+    Component("App2", ({ el }) => {
+      const appEl = el(div);
+
+      setTimeout(() => {
+        expect(appEl.it).toBe(div);
+        done();
+      }, 200);
+
+      return () => ``;
+    }).render();
+  });
+
+  it("Should get element from template and use in ManagerEl", (done) => {
+    Component("App3", ({ el }) => {
+      const [appEl, key] = el();
+
+      setTimeout(() => {
+        expect(appEl.it?.tagName?.toLowerCase()).toBe("button");
+        done();
+      }, 200);
+
+      return () => `button[ ${key} Click me!]`;
+    }).render();
+  });
+
+  it("Should be null", (done) => {
+    Component("App4", ({ el }) => {
+      const [appEl] = el();
+
+      setTimeout(() => {
+        expect(appEl.it).toBeNull();
+        done();
+      }, 200);
+
+      return () => `button[Click me!]`;
+    }).render();
+  });
+
+  it("Should replace the HTML elements “span” with “strong”", (done) => {
+    Component("App5", ({ el }) => {
+      const [appEl, key] = el();
+      let t = `span[${key} class="today" ~ Click me!]`;
+
+      setTimeout(() => {
+        t = `strong[${key} class="today" ~ Click me!]`;
+      }, 100);
+
+      setTimeout(() => {
+        expect(appEl.it?.tagName?.toLowerCase()).toBe("strong");
+        done();
+      }, 200);
+
+      return () => t;
+    }).render();
+  });
+
+  it("Should update element attributes", (done) => {
+    Component("App6", ({ el }) => {
+      const [appEl, key] = el();
+      let t = `span[${key} class="today" color:red; ~ Click me!]`;
+
+      setTimeout(() => {
+        t = `span[${key} class="today tomorrow" color:red; ~ Click me!]`;
+      }, 100);
+
+      setTimeout(() => {
+        expect(appEl.it?.tagName?.toLowerCase()).toBe("span");
+        done();
+      }, 200);
+
+      return () => t;
+    }).render();
+  });
+});
--- __tests__/bemtv/manager-el.test.ts
+++ __tests__/bemtv/manager-el.test.ts
@@ -1,71 +1,9 @@
 import { Component } from "../../src/main";
 
 describe("ManagerEl", () => {
-  describe("ManagerEl.it property", () => {
-    it("Should use #app element in ManagerEl", (done) => {
-      const div = document.createElement("div");
-      div.id = "app";
-      document.body.appendChild(div);
-
-      Component("App", ({ el }) => {
-        const appEl = el("#app");
-
-        setTimeout(() => {
-          expect(appEl.it).toBe(div);
-          done();
-        }, 200);
-
-        return () => ``;
-      }).render();
-    });
-
-    it("Should use div element in ManagerEl", (done) => {
-      const div = document.createElement("div");
-
-      document.body.appendChild(div);
-
-      Component("App1", ({ el }) => {
-        const appEl = el(div);
-
-        setTimeout(() => {
-          expect(appEl.it).toBe(div);
-          done();
-        }, 200);
-
-        return () => ``;
-      }).render();
-    });
-
-    it("Should get element from template and use in ManagerEl", (done) => {
-      Component("App2", ({ el }) => {
-        const [appEl, key] = el();
-
-        setTimeout(() => {
-          expect(appEl.it?.tagName?.toLowerCase()).toBe("button");
-          done();
-        }, 200);
-
-        return () => `button[ ${key} Click me!]`;
-      }).render();
-    });
-
-    it("Should be null", (done) => {
-      Component("App3", ({ el }) => {
-        const [appEl] = el();
-
-        setTimeout(() => {
-          expect(appEl.it).toBeNull();
-          done();
-        }, 200);
-
-        return () => `button[Click me!]`;
-      }).render();
-    });
-  });
-
-  describe("ManagerEl.it method", () => {
+  describe("ManagerEl.css method", () => {
     it("Should add style to element", (done) => {
-      Component("App4", ({ el }) => {
+      Component("App1", ({ el }) => {
         const [appEl, key] = el<HTMLButtonElement>();
 
         appEl.css`font-size:50px;`;
@@ -89,7 +27,7 @@ describe("ManagerEl", () => {
 
   describe("Inject event handlers to ManagerEl instance", () => {
     it("Should add onclick event listener to element", (done) => {
-      Component("App5", ({ el }) => {
+      Component("App2", ({ el }) => {
         const [appEl, key] = el<HTMLButtonElement>();
         const clickFn = jest.fn();
 
@@ -110,7 +48,7 @@ describe("ManagerEl", () => {
     });
 
     it("Should remove onclick event listener from element", (done) => {
-      Component("App6", ({ el }) => {
+      Component("App3", ({ el }) => {
         const [appEl, key] = el<HTMLButtonElement>();
         const clickFn = jest.fn();
 
