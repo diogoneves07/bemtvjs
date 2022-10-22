1e7d16a56dc47aeddc330aeaaea7196d1dcbb68d
diogoneves07 <ndiogo778@gmail.com>
2022-10-22 11:15:13 -0300

    ci: add CI with github-actions

---
+++ src/bemtv/types/component-this-data.ts
@@ -0,0 +1,19 @@
+import { ComponentThis } from "../components-this";
+import { ManagerEl } from "../manager-el";
+import { ComponentListener } from "./listeners";
+
+export type Props = Record<string, any>;
+export type LifeCycleCallback = () => void;
+
+export interface ComponentThisData {
+  listeners: Set<ComponentListener>;
+  firstElement: Element | null;
+  propsDefined?: Map<string, Props>;
+  mountedFns: Set<LifeCycleCallback>;
+  unmountedFns?: Set<LifeCycleCallback>;
+  updatedFns: Set<LifeCycleCallback>;
+  mounted: boolean;
+  els: ManagerEl[];
+  sharedData: Record<string, any>;
+  parent: ComponentThis | null;
+}
---
+++ src/bemtv/types/listeners.ts
@@ -0,0 +1,22 @@
+export type EventMap = WindowEventMap &
+  DocumentEventMap &
+  HTMLElementEventMap &
+  SVGElementEventMap;
+
+export type RemoveListener = () => void;
+export type ListenerCallback<Event> = (
+  fn: (event: Event) => void,
+  options?: AddEventListenerOptions
+) => RemoveListener;
+
+export type Listeners = {
+  [Property in keyof EventMap as `${string & Property}$`]: ListenerCallback<
+    EventMap[Property]
+  >;
+};
+
+export type ComponentListener = {
+  listener: string;
+  args: [fn: Function, options: AddEventListenerOptions];
+  removeListener?: () => void;
+};
