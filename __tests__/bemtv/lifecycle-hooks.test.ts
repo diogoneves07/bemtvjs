60a0a6afcd17a90fa0c74d9977aa14f6fd74f6d6
diogoneves07 <ndiogo778@gmail.com>
2022-10-24 12:15:04 -0300

    test: all lifecycle hooks

--- __tests__/bemtv/lifecycle-hooks.test.ts
+++ __tests__/bemtv/lifecycle-hooks.test.ts
@@ -4,6 +4,37 @@ import { resetTestEnvironment } from "./utilities/reset-test-environment";
 resetTestEnvironment();
 
 describe("Lifecycle Hooks", () => {
+  test("onMount method", (done) => {
+    const onMountFn = jest.fn();
+
+    Component("App", ({ onMount }) => {
+      onMount(onMountFn);
+      return "Hello";
+    }).render();
+
+    setTimeout(() => {
+      expect(onMountFn).toBeCalledTimes(1);
+      done();
+    }, 100);
+  });
+
+  test("onUpdate method", (done) => {
+    const onUpdateFn = jest.fn();
+
+    Component("App", ({ onUpdate }) => {
+      onUpdate(onUpdateFn);
+
+      let t = `Hello`;
+      setTimeout(() => (t = "Hey!"), 100);
+      return () => t;
+    }).render();
+
+    setTimeout(() => {
+      expect(onUpdateFn).toBeCalledTimes(1);
+      done();
+    }, 200);
+  });
+
   test("onUnmount method", (done) => {
     const onUnmountFn = jest.fn();
 
