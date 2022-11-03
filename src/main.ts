de93e4ef5365e7ccb8895e51174b459b1c98996b
diogoneves07 <ndiogo778@gmail.com>
2022-11-03 10:52:21 -0300

    feat: add pipe

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
+export { pipe } from "./bemtv/pipes/main";
+export {
+  orderedListPipe as olPipe,
+  unorderedListPipe as ulPipe,
+} from "./bemtv/pipes/html-list-pipes";
+export { descriptionListPipe as dlPipe } from "./bemtv/pipes/description-List-pipe";
+
 export { matchComponent as match } from "./bemtv/match-component";
 export { autoImportComponents } from "./bemtv/auto-import-components";
 export type { ManagerEl } from "./bemtv/manager-el";
 export type { ComponentThis } from "./bemtv/components-this";
-export { Component, _, hasComponent } from "./bemtv/components";
+export { Component, _, hasComponent } from "./bemtv/components-main";
 export { default as render } from "./bemtv/render";
 export { router, r } from "./router/main";
