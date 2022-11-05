4fc5c93e03591b7706505b2f7c3dc9fa62e3d9dd
diogoneves07 <ndiogo778@gmail.com>
2022-11-05 08:45:31 -0300

    test: use SuperComponent

---
+++ __tests__/bemtv/sharing-api.test.ts
@@ -0,0 +1,132 @@
+import { Component } from "../../src/main";
+import { resetTestEnvironment } from "../test-utilities/reset-test-environment";
+
+resetTestEnvironment();
+
+describe("sharing API", () => {
+  describe("method share()", () => {
+    it('Should share "message" property', (done) => {
+      (() => {
+        const { use, template } = Component("Child");
+
+        template(() => `${use("message")}`);
+      })();
+
+      const { share, onMount, template, render } = Component("App");
+
+      share({ message: "Hello" });
+
+      onMount(() => {
+        expect(document.body.textContent?.trim()).toBe("Hello");
+        done();
+      });
+      template(() => `Child[]`);
+
+      render();
+    });
+
+    test('Parent should not access the "user" property', (done) => {
+      (() => {
+        const { share } = Component("Child");
+
+        share({ user: "World" });
+      })();
+
+      const { use, onMount, template, render } = Component("App");
+
+      onMount(() => {
+        expect(document.body.textContent?.trim()).toBe("undefined");
+        done();
+      });
+
+      template(() => `Child[] ${use("user")}`);
+
+      render();
+    });
+  });
+  describe("method reshare()", () => {
+    it('Should update "message" property', (done) => {
+      (() => {
+        const { reshare } = Component("Child");
+
+        reshare({ message: "Hey!" });
+      })();
+
+      const { share, use, onMount, template, render } = Component("App");
+
+      share({ message: "" });
+
+      onMount(() => {
+        expect(document.body.textContent?.trim()).toBe("Hey!");
+        done();
+      });
+
+      template(() => `${use("message")} Child[]`);
+
+      render();
+    });
+
+    it('Should not update "message" property', (done) => {
+      (() => {
+        const { share } = Component("Child");
+
+        share({ message: "Hey!" });
+      })();
+
+      const { reshare, use, onMount, template, render } = Component("App");
+
+      reshare({ message: "" });
+
+      onMount(() => {
+        expect(document.body.textContent?.trim()).toBe("undefined");
+        done();
+      });
+
+      template(() => `${use("message")} Child[]`);
+
+      render();
+    });
+  });
+
+  describe("method use()", () => {
+    it('Should use "message" property', (done) => {
+      (() => {
+        const { use, template } = Component("Child");
+
+        template(() => `${use<string>("message")}`);
+      })();
+
+      const { share, onMount, template, render } = Component("App");
+
+      share({ message: "Hey" });
+
+      onMount(() => {
+        expect(document.body.textContent?.trim()).toBe("Hey");
+        done();
+      });
+
+      template(() => `Child[]`);
+
+      render();
+    });
+
+    it('Should not use "message" property', (done) => {
+      (() => {
+        const { share } = Component("Child");
+
+        share({ message: "Hey" });
+      })();
+
+      const { use, onMount, template, render } = Component("App");
+
+      onMount(() => {
+        expect(document.body.textContent?.trim()).toBe("undefined");
+        done();
+      });
+
+      template(() => `Child[] ${use("message")}`);
+
+      render();
+    });
+  });
+});
