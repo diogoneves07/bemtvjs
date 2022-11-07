518d2c5ed03fd18d9a8fae7d3c824e5702304e66
diogoneves07 <ndiogo778@gmail.com>
2022-11-07 08:34:17 -0300

    test: use mock to advance requestAnimationFrame

--- __tests__/bemtv/non-existent-component.test.ts
+++ __tests__/bemtv/non-existent-component.test.ts
@@ -1,5 +1,12 @@
 import { Component } from "../../src/main";
 
+beforeEach(() => {
+  jest.spyOn(window, "requestAnimationFrame").mockImplementation((fn) => {
+    fn(Date.now());
+    return 1;
+  });
+});
+
 it("Should throw an error because the component does not exit", () => {
   expect(() => {
     Component("App").template`NotExist[]`.render();
