28ee02de8ab0d3de3e140dc72a4f9efa5aae3cbc
diogoneves07 <ndiogo778@gmail.com>
2022-12-30 23:22:34 -0300

    perf: remove unnecessary code and reorganize

--- src/bemtv/request-animation-frame-loop.ts
+++ src/bemtv/request-animation-frame-loop.ts
@@ -1,5 +1,4 @@
 import { ALL_COMPONENTS_INST } from "./component-inst-store";
-import ComponentInst from "./component-inst";
 import { setComponentManagerNodes } from "./components-inst-nodes";
 import updatedUIWithNewTemplate from "./update-ui-with-new-template";
 import {
@@ -9,58 +8,59 @@ import {
 import { BRACKETHTML_CSS_IN_JS } from "../brackethtml/globals";
 import shouldComponentBeUnmounted from "./should-component-be-unmounted";
 
-const componentsToDelete: Set<ComponentInst> = new Set();
-
 const framesLimit = 20;
 
 const timeoutForLoop = 1000 / framesLimit;
 
-(function requestAnimationFrameLoop() {
-  componentsToDelete.clear();
-
-  requestAnimationFrame(() => {
-    let hasChanges = false;
-    for (const componentInst of ALL_COMPONENTS_INST) {
-      if (!componentInst.parentElement) continue;
-
-      if (!componentInst.shouldTemplateBeUpdate()) {
-        shouldComponentBeUnmounted(componentInst);
-        continue;
-      }
-
-      hasChanges = true;
-
-      shouldComponentBeUnmounted(componentInst);
-
-      const updatedUI = updatedUIWithNewTemplate(componentInst);
-
-      if (updatedUI) {
-        const { newComponentsManager, componentsManagerUpdated, keysAndNodes } =
-          updatedUI;
-
-        if (
-          !componentInst.shouldForceUpdate ||
-          newComponentsManager.length ||
-          componentsManagerUpdated.length
-        ) {
-          dispatchUpdatedLifeCycle(componentInst);
-        }
-
-        for (const c of componentsManagerUpdated) {
-          dispatchUpdatedLifeCycle(c);
-        }
-
-        for (const c of newComponentsManager) {
-          if (keysAndNodes[c.key]) {
-            setComponentManagerNodes(c.key, keysAndNodes[c.key]);
-          }
-          dispatchMountedLifeCycle(c);
-        }
-      }
+function requestAnimationFrameLoop() {
+  let hasChanges = false;
+  for (const componentInst of ALL_COMPONENTS_INST) {
+    if (!componentInst.parentElement) {
+      continue;
     }
 
-    if (hasChanges) BRACKETHTML_CSS_IN_JS.applyLastCSSCreated();
+    if (!componentInst.shouldTemplateBeUpdate()) {
+      shouldComponentBeUnmounted(componentInst);
+      continue;
+    }
 
-    setTimeout(() => requestAnimationFrameLoop(), timeoutForLoop);
-  });
-})();
+    hasChanges = true;
+
+    shouldComponentBeUnmounted(componentInst);
+
+    const updatedUI = updatedUIWithNewTemplate(componentInst);
+
+    if (updatedUI) {
+      const { newComponentsManager, componentsManagerUpdated, keysAndNodes } =
+        updatedUI;
+
+      if (
+        !componentInst.shouldForceUpdate ||
+        newComponentsManager.length ||
+        componentsManagerUpdated.length
+      ) {
+        dispatchUpdatedLifeCycle(componentInst);
+      }
+
+      for (const c of componentsManagerUpdated) {
+        dispatchUpdatedLifeCycle(c);
+      }
+
+      for (const c of newComponentsManager) {
+        if (keysAndNodes[c.name]) {
+          setComponentManagerNodes(c.name, keysAndNodes[c.name]);
+        }
+        dispatchMountedLifeCycle(c);
+      }
+    }
+  }
+
+  if (hasChanges) BRACKETHTML_CSS_IN_JS.applyLastCSSCreated();
+}
+
+requestAnimationFrame(requestAnimationFrameLoop);
+
+setInterval(
+  () => requestAnimationFrame(requestAnimationFrameLoop),
+  timeoutForLoop
+);
