f6b76f5430f37898674cacfaa50c3b0ce0f39082
diogoneves07 <ndiogo778@gmail.com>
2022-12-30 23:34:23 -0300

    fix: must check the “unmounted” property to check that the component's parent was being unmounted

--- src/bemtv/should-component-be-unmounted.ts
+++ src/bemtv/should-component-be-unmounted.ts
@@ -6,11 +6,17 @@ export default function shouldComponentBeUnmounted(
   componentInst: ComponentInst
 ) {
   if (!componentInst.parent) return false;
-  if (componentInst.parent.hasComponentChild(componentInst)) return false;
 
-  dispatchUnmountedLifeCycle(componentInst);
+  if (
+    !componentInst.parent.unmounted &&
+    componentInst.parent.hasComponentChild(componentInst)
+  ) {
+    return false;
+  }
 
   ALL_COMPONENTS_INST.delete(componentInst);
+
+  dispatchUnmountedLifeCycle(componentInst);
 
   return true;
 }
