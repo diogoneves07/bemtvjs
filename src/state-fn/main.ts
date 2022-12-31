2433041c848a007dcd25e590c14814bd83b30e61
diogoneves07 <ndiogo778@gmail.com>
2022-12-30 23:04:25 -0300

    feat: new “stateFn” to improve the state management experience between components

---
+++ src/state-fn/main.ts
@@ -0,0 +1,60 @@
+import { LIBRARY_NAME_IN_ERRORS_MESSAGE } from "../globals";
+
+type WatchCallback<V = any> = (value: V, count: number) => void;
+type RemoveWatcher = () => void;
+
+type StateFnLib<V> = {
+  (newValue?: V): V;
+  $watch: (callback: WatchCallback<V>) => RemoveWatcher;
+};
+
+type StateFn<T, A extends Record<string, (c: T, n: T) => T>> = StateFnLib<T> & {
+  [K in keyof A]: (value: Parameters<A[K]>[1]) => T;
+};
+
+export function stateFn<T, A extends Record<string, (c: T, n: T) => T>>(
+  value: T,
+  actions?: A
+) {
+  const watchers = new Map<WatchCallback, number>();
+
+  let currentValue = value;
+
+  const fn = (newValue?: T) => {
+    if (actions) {
+      console.error(value);
+      throw `${LIBRARY_NAME_IN_ERRORS_MESSAGE} The state funtion that init with the value above can only have its value changed through “actions”.`;
+    }
+
+    if (newValue === undefined) return currentValue;
+
+    currentValue = newValue;
+
+    watchers.forEach((c, fn) => {
+      const count = c++;
+      fn(newValue, count);
+      watchers.set(fn, count);
+    });
+
+    return newValue;
+  };
+
+  fn.$watch = (fn: WatchCallback) => {
+    fn(currentValue, 0);
+
+    !watchers.has(fn) && watchers.set(fn, 0);
+
+    return (() => watchers.delete(fn)) as RemoveWatcher;
+  };
+
+  if (actions) {
+    Object.keys(actions).forEach((k) => {
+      (fn as any)[k] = (value: T) => {
+        currentValue = actions[k](currentValue, value);
+        return currentValue;
+      };
+    });
+  }
+
+  return fn as StateFn<T, A>;
+}
