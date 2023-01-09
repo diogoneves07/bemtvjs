19b53e51c5bbfc0c54c200ba3e83aa978bc82bf2
diogoneves07 <ndiogo778@gmail.com>
2023-01-09 08:13:29 -0300

    fix: you must use the “template” element to manipulate the HTML without markup prohibitions

--- src/brackethtml/is-self-closing-tag.ts
+++ src/brackethtml/is-self-closing-tag.ts
@@ -1,10 +1,18 @@
+import { LIBRARY_NAME_IN_ERRORS_MESSAGE } from "../globals";
+
 const CACHE: Record<string, boolean> = {};
-const SIMPLE_DIV = document.createElement("div");
+
+const templateElement = document.createElement("template");
+
+if (!("content" in templateElement)) {
+  throw `${LIBRARY_NAME_IN_ERRORS_MESSAGE} The HTML “template” element is not supported by this browser, please use a newer version of this browser.`;
+}
+
 export default function isSelfClosingTag(tag: string) {
   if (CACHE[tag]) return CACHE[tag];
-  SIMPLE_DIV.innerHTML = `<${tag}></${tag}>`;
+  templateElement.innerHTML = `<${tag}></${tag}>`;
 
-  const element = SIMPLE_DIV.children[0];
+  const element = templateElement.content.firstElementChild as Element;
   const check = !element.outerHTML.includes(`</${tag}>`);
 
   CACHE[tag] = check;
