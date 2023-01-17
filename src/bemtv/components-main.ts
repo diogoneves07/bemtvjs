615af55d3940aff213d32606817b9ea498c785ae
diogoneves07 <ndiogo778@gmail.com>
2023-01-17 11:33:11 -0300

    refactor: remove functional components
    BREAKING CHANGE: syntax for functional components has been deprecated

--- src/bemtv/components-main.ts
+++ src/bemtv/components-main.ts
@@ -1,25 +1,15 @@
 import { SuperComponent } from "./super-component/super-component";
 import { LIBRARY_NAME_IN_ERRORS_MESSAGE } from "../globals";
 import { SuperComponentFactory } from "./super-component/super-component-factory";
-import { bindComponentToSuperComponent } from "./super-component/bind-comp-to-s-comp";
-import ComponentInst from "./component-inst";
-import render from "./render";
 import concatTemplateStringArrays from "../utilities/concat-template-string-arrays";
+import { getSuperComponentData } from "./super-component/work-with-super-component";
 
-type GlobalComponentsMap = Map<string, ComponentFn>;
+export const ALL_SUPER_COMPONENTS: SuperComponent[] = [];
 
-type ComponentName = `${Capitalize<string>}${string}` | string;
-
-export const GLOBAL_COMPONENTS_MAP: GlobalComponentsMap = new Map();
-
-export type ComponentFn =
-  | ((self: ComponentInst) => () => string)
-  | ((self: ComponentInst) => string);
-
-export type ComponentTemplateCallback = () => string;
-
-export function getComponentFn(componentName: string) {
-  return GLOBAL_COMPONENTS_MAP.get(componentName);
+export function getSuperComponentInst(componentName: string) {
+  return ALL_SUPER_COMPONENTS.find(
+    (s) => getSuperComponentData(s).componentName === componentName
+  );
 }
 
 /**
@@ -28,28 +18,9 @@ export function getComponentFn(componentName: string) {
  * @param name
  * The component name.
  */
-export function hasComponent(name: ComponentName) {
-  return GLOBAL_COMPONENTS_MAP.get(name) ? true : false;
+export function hasComponent(name: string) {
+  return getSuperComponentInst(name) ? true : false;
 }
-
-class ComponentsInst {
-  protected __name: string;
-  constructor(name: string) {
-    this.__name = name;
-  }
-  /**
-   * Renders the component somewhere on the page.
-   *
-   * @param selectorOrElement
-   * The element to insert the nodes.
-   */
-  render(selectorOrElement: string | Element = document.body) {
-    render(`${this.__name}[]`, selectorOrElement);
-    return this;
-  }
-}
-
-type SuperComponentFn = (c: SuperComponent) => void;
 
 /**
  * Defines the component name.
@@ -60,7 +31,7 @@ type SuperComponentFn = (c: SuperComponent) => void;
  * alphanumeric characters and the `:` symbol.
  *
  * @returns
- * A function to define the component type;
+ * A function to define the component vars.
  */
 
 export function _(componentName: TemplateStringsArray, ...args: string[]) {
@@ -71,64 +42,27 @@ export function _(componentName: TemplateStringsArray, ...args: string[]) {
       : n
   ) as string;
 
-  function compType(): SuperComponent;
-
-  function compType(compFn: SuperComponentFn): ComponentsInst;
-
-  function compType<V extends Record<string, any>>(
-    compVars: V
-  ): SuperComponent<V>;
+  if (getSuperComponentInst(name)) {
+    throw `${LIBRARY_NAME_IN_ERRORS_MESSAGE} The “${name}” component already exists!`;
+  }
 
   /**
    * Finishes the creation of the component
    * and provides access to an instance of SuperComponent.
    */
-  function compType(compVarsOrCompFn?: Record<string, any> | SuperComponentFn) {
-    if (typeof compVarsOrCompFn === "function") {
-      const compFn = compVarsOrCompFn;
-
-      if (GLOBAL_COMPONENTS_MAP.has(name)) {
-        throw `${LIBRARY_NAME_IN_ERRORS_MESSAGE} The “${name}” component already exists!`;
-      }
-
-      GLOBAL_COMPONENTS_MAP.set(name, (c) => {
-        const s = SuperComponentFactory(name);
-
-        compFn(s);
-
-        return bindComponentToSuperComponent(s, c);
-      });
-
-      return new Proxy(new ComponentsInst(name), {
-        get(target, n) {
-          const propName = n as string;
-          const t = target as any;
-
-          if (propName in target) {
-            if (typeof t[propName] === "function") {
-              return t[propName].bind(target);
-            }
-            return t[propName];
-          }
-        },
-      });
-    }
-
-    const vars = compVarsOrCompFn || {};
-
-    if (GLOBAL_COMPONENTS_MAP.has(name)) {
-      throw `${LIBRARY_NAME_IN_ERRORS_MESSAGE} The “${name}” component already exists!`;
-    }
+  function defineVars<CompVars extends Record<string, any>>(
+    compVars?: CompVars
+  ) {
+    const vars = (compVars || {}) as CompVars;
 
     Object.freeze(vars);
 
-    const superComponent = SuperComponentFactory(name, vars);
-    GLOBAL_COMPONENTS_MAP.set(name, (c) =>
-      bindComponentToSuperComponent(superComponent, c)
-    );
+    const superComponent = SuperComponentFactory<CompVars>(name, vars);
+
+    ALL_SUPER_COMPONENTS.push(superComponent);
 
     return superComponent;
   }
 
-  return compType;
+  return defineVars;
 }
