ebea93184fb1986b990494c4e4a45dfe33a652d8
diogoneves07 <ndiogo778@gmail.com>
2023-01-18 23:54:19 -0300

    refactor: handle the “onInst()” method

--- src/bemtv/super-component/super-component-factory.ts
+++ src/bemtv/super-component/super-component-factory.ts
@@ -27,6 +27,8 @@ export function SuperComponentFactory<Vars extends Record<string, any>>(
 
       const t = target as Record<string, any>;
 
+      if (propName === "onInst") return t[propName];
+
       if (propName.slice(0, 2) === "on") {
         if (t[propName]) return t[propName];
 
